a999c52956f8c9085d907aaa082cdf5b506d395c
Noodle98 <46034007+Noodle98@users.noreply.github.com>
2023-12-05 12:46:51 +0100

    S2010_gui_exercise1.py more progress

--- 10_gui/S2010_gui_exercise1.py
+++ 10_gui/S2010_gui_exercise1.py
@@ -12,20 +12,32 @@ Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
 Fortsæt derefter med den næste fil."""
 
 import tkinter as tk
-class main_window(tk.Tk):
+from tkinter import ttk
+
+
+class Main_window(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        self.wm_title(self, "my first GUI")
-
-        main_grid = self.grid()
-
+        self.wm_title("my first GUI")
+        my_frame = tk.Frame(self, bd=1, relief="solid")
+        button1 = tk.Button(my_frame, text="Create")
+        my_frame.grid()
+        button1.pack()
 
 
 window = tk.Tk()
 window.title("my first GUI")
-window.geometry("300x500")
+window.geometry("150x200")
+frame = tk.Frame(window, borderwidth=1, border=10)
+frame.pack(fill="both", expand=True)
+
+label_id = tk.Label(frame, text="Id")
+entry1 = tk.Entry(frame, width=5)
+button_create = tk.Button(frame, text="Create")
+
+label_id.grid()
+entry1.grid()
+button_create.grid()
 
 
-
-if __name__ == "__main__":
-    window.mainloop()
+window.mainloop()
