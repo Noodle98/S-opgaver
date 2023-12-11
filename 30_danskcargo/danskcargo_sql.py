412fc1c4f1868ed3e1295d14f4a023c00c6d23dd
Noodle98 <46034007+Noodle98@users.noreply.github.com>
2023-12-11 14:57:08 +0100

    danskcargo opgave progress

--- 30_danskcargo/danskcargo_sql.py
+++ 30_danskcargo/danskcargo_sql.py
@@ -5,11 +5,13 @@ from danskcargo_data import Container, Base
 # add the following 7 lines to make foreign key constraints work  https://docs.sqlalchemy.org/en/14/dialects/sqlite.html#sqlite-foreign-keys
 from sqlalchemy.engine import Engine
 from sqlalchemy import event
+
+
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
+
 
 Database = 'sqlite:///danskcargo.db'  # first part: database type, second part: file path
 
