0612067f849626908a0bbf78569b2222763ddc39
Noodle98 <46034007+Noodle98@users.noreply.github.com>
2024-01-10 15:03:01 +0100

    danskcargo progress 3

--- 30_danskcargo/danskcargo_data.py
+++ 30_danskcargo/danskcargo_data.py
@@ -28,3 +28,57 @@ class Container(Base):
     def convert_from_tuple(tuple_):  # Convert tuple to Container
         container = Container(id=tuple_[0], weight=tuple_[1], destination=tuple_[2])
         return container
+
+
+class Aircraft(Base):
+    __tablename__ = "aircraft"
+    id = Column(Integer, primary_key=True)
+    max_cargo_weight = Column(Integer)
+    registration = Column(String)
+
+    def __repr__(self):  # Optional. Only for test purposes.
+        return f"Aircraft({self.id=:4}    {self.max_cargo_weight=:5}    {self.registration=})"
+
+    def convert_to_tuple(self):  # Convert Container to tuple
+        return self.id, self.max_cargo_weight, self.registration
+
+    def valid(self):
+        try:
+            value = int(self.max_cargo_weight)
+        except ValueError:
+            return False
+        return value >= 0
+
+    @staticmethod
+    def convert_from_tuple(tuple_):  # Convert tuple to Container
+        aircraft = Aircraft(id=tuple_[0], max_cargo_weight=tuple_[1], registration=tuple_[2])
+        return aircraft
+
+
+class Transport(Base):
+    __tablename__ = "transport"
+    id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    container_id = Column(Integer, ForeignKey("container.id"), nullable=False)
+    aircraft_id = Column(Integer, ForeignKey("aircraft.id"), nullable=False)
+
+    def __repr__(self):  # Optional. Only for test purposes.
+        return f"Transport({self.id=:4}    {self.date=:5}    {self.container_id=}    {self.aircraft_id=})"
+
+    def convert_to_tuple(self):  # Convert Container to tuple
+        return self.id, self.date, self.container_id, self.aircraft_id
+
+    def valid(self):
+        try:
+            value = int(self.date)
+        except ValueError:
+            return False
+        return value >= 0
+
+    @staticmethod
+    def convert_from_tuple(tuple_):  # Convert tuple to Container
+        date = parser.parse(tuple_[1])
+        container_id = int(tuple_[2])
+        aircraft_id = int(tuple_[3])
+        transport = Transport(id=tuple_[0], date=date, container_id=container_id, aircraft_id=aircraft_id)
+        return transport
--- 30_danskcargo/danskcargo_sql.py
+++ 30_danskcargo/danskcargo_sql.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, select, update, delete
-from danskcargo_data import Container, Base
+from datetime import date
+from danskcargo_data import Container, Aircraft, Transport, Base
 
 # add the following 7 lines to make foreign key constraints work  https://docs.sqlalchemy.org/en/14/dialects/sqlite.html#sqlite-foreign-keys
 from sqlalchemy.engine import Engine as engine
@@ -19,10 +20,14 @@ Database = 'sqlite:///danskcargo.db'  # first part: database type, second part: 
 def create_test_data():  # Optional. Used to test database functions before gui is ready.
     with Session(engine) as session:
         new_items = []
-        new_items.append(Container(weight=1200, destination="Oslo"))
-        new_items.append(Container(weight=700, destination="Helsinki"))
-        new_items.append(Container(weight=1800, destination="Helsinki"))
-        new_items.append(Container(weight=1000, destination="Helsinki"))
+        # new_items.append(Container(weight=1200, destination="Oslo"))
+        # new_items.append(Container(weight=700, destination="Helsinki"))
+        # new_items.append(Container(weight=1800, destination="Helsinki"))
+        # new_items.append(Container(weight=1000, destination="Helsinki"))
+        new_items.append(Aircraft(max_cargo_weight=2000, registration="OY_CBS"))
+        new_items.append(Aircraft(max_cargo_weight=3000, registration="OY-THR"))
+        a_date = date(day=10, month=12, year=2022)
+        new_items.append(Transport(date=a_date, container_id=2, aircraft_id=1))
         session.add_all(new_items)
         session.commit()
 
@@ -76,6 +81,51 @@ def delete_soft_container(container):
 # endregion container
 
 
+# region aircraft
+def update_aircraft(aircraft):  # https://docs.sqlalchemy.org/en/14/tutorial/orm_data_manipulation.html#orm-enabled-update-statements
+    # update a record in the aircraft table
+    with Session(engine) as session:
+        session.execute(update(Aircraft).where(Aircraft.id == aircraft.id).values(max_cargo_weight=aircraft.max_cargo_weight, registration=aircraft.registration))
+        session.commit()  # makes changes permanent in database
+
+
+def delete_hard_container(aircraft):
+    # delete a record in the aircraft table
+    with Session(engine) as session:
+        session.execute(delete(Aircraft).where(Aircraft.id == aircraft.id))
+        session.commit()  # makes changes permanent in database
+
+
+def delete_soft_container(aircraft):
+    # soft delete a record in the aircraft table by setting its weight to -1 (see also method "valid" in the container class)
+    with Session(engine) as session:
+        session.execute(update(Aircraft).where(Aircraft.id == aircraft.id).values(max_cargo_weight=-1, registration=aircraft.registration))
+        session.commit()  # makes changes permanent in database
+# endregion aircraft
+
+
+# region transport
+def update_transport(transport):  # https://docs.sqlalchemy.org/en/14/tutorial/orm_data_manipulation.html#orm-enabled-update-statements
+    # update a record in the transport table
+    with Session(engine) as session:
+        session.execute(update(Transport).where(Transport.id == transport.id).values(date=transport.date, container_id=transport.container_id, aircraft_id=transport.aircraft_id))
+        session.commit()  # makes changes permanent in database
+
+
+def delete_hard_transport(transport):
+    # delete a record in the transport table
+    with Session(engine) as session:
+        session.execute(delete(Transport).where(Transport.id == transport.id))
+        session.commit()  # makes changes permanent in database
+
+
+def delete_soft_transport(transport):
+    # soft delete a record in the transport table by setting its weight to -1 (see also method "valid" in the container class)
+    with Session(engine) as session:
+        session.execute(update(Transport).where(Transport.id == transport.id).values(date=-1, container_id=transport.container_id, aircraft_id=transport.aircraft_id))
+        session.commit()  # makes changes permanent in database
+# endregion transport
+
 if __name__ == "__main__":  # Executed when invoked directly
 # The next 2 lines are needed _after_ data classes / sql tables were defined
     engine = create_engine(Database, echo=False, future=True)
@@ -86,3 +136,5 @@ if __name__ == "__main__":  # Executed when invoked directly
 else:  # Executed when imported
     engine = create_engine(Database, echo=False, future=True)  # https://docs.sqlalchemy.org/en/14/tutorial/engine.html   The start of any SQLAlchemy application is an object called the Engine. This object acts as a central source of connections to a particular database, providing both a factory as well as a holding space called a connection pool for these database connections. The engine is typically a global object created just once for a particular database server, and is configured using a URL string which will describe how it should connect to the database host or backend.
     Base.metadata.create_all(engine)
+
+#create_test_data()
