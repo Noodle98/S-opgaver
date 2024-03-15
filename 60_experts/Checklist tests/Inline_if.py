5ca405f65847024c14f6431f6c707acba8be1007
Noodle98 <46034007+Noodle98@users.noreply.github.com>
2024-03-15 11:53:27 +0100

    Advanced inline if test progress

--- 60_experts/Checklist tests/Inline_if.py
+++ 60_experts/Checklist tests/Inline_if.py
@@ -2,3 +2,7 @@
 # Kør programmet og leg med koden
 q = "a" if 3 > 8 else "b"
 print(q)
+my_array = [2, 3, 5, 10, 8]
+new_array = [x if x > 5 else 1 for x in my_array]
+for x in new_array:
+    print(x)
